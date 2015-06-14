7fc77d6405528a24d4efcc51ae396180b2609b56
sherlockHK <576717731@qq.com>
2015-06-14 11:35:20 +0800

    add movieStore

--- test/com/twu/biblioteca/MovieStoreTest.java
+++ test/com/twu/biblioteca/MovieStoreTest.java
@@ -1,7 +1,13 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
+
 /**
  * Created by Administrator on 2015/6/14.
  */
 public class MovieStoreTest {
+    private MovieStore movieStore;
+
+    @Before
+    public void setUp(){movieStore = new MovieStore();}
 }
